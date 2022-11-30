715686e6aa6b3f56eb27df7a8a471d088f45be6f
ObscuriaLithium <ObscuriaLithium@gmail.com>
2022-12-01 00:16:42 +0300

    update

--- src/main/java/com/obscuria/obscuretooltips/ModConfig.java
+++ src/main/java/com/obscuria/obscuretooltips/ModConfig.java
@@ -16,28 +16,15 @@ public class ModConfig {
     public static class Client {
         public static final ForgeConfigSpec.Builder BUILDER = new ForgeConfigSpec.Builder();
         public static final ForgeConfigSpec CLIENT_SPEC;
-        public static final ForgeConfigSpec.BooleanValue displayFoodIcons;
-        public static final ForgeConfigSpec.BooleanValue displayEquipmentIcons;
-        public static final ForgeConfigSpec.BooleanValue reduceTooltips;
-        public static final ForgeConfigSpec.IntValue buttonsOffsetX;
-        public static final ForgeConfigSpec.BooleanValue pickupDisplay;
-        public static final ForgeConfigSpec.BooleanValue pickupStyle;
-        public static final ForgeConfigSpec.IntValue buttonsOffsetY;
-        public static final ForgeConfigSpec.IntValue pickupLifetime;
+        public static final ForgeConfigSpec.BooleanValue showArmorPreview;
+        public static final ForgeConfigSpec.BooleanValue renderModel;
+        public static final ForgeConfigSpec.DoubleValue scale;
 
         static {
-            BUILDER.push("PickupNotifier");
-            pickupDisplay = BUILDER.worldRestart().define("Display2", true);
-            pickupStyle = BUILDER.worldRestart().define("Style", true);
-            pickupLifetime = BUILDER.worldRestart().defineInRange("Lifetime", 5, 0, 20);
-
-            BUILDER.pop();
-            BUILDER.push("Tooltips");
-            displayFoodIcons = BUILDER.comment("Display food properties in tooltips").worldRestart().define("Food Icons", true);
-            displayEquipmentIcons = BUILDER.comment("Display equipment attributes in tooltips").worldRestart().define("Equipment Icons", true);
-            reduceTooltips = BUILDER.comment("Display reduced tooltips for new inventory buttons").worldRestart().define("Small Tooltips", true);
-            buttonsOffsetX = BUILDER.worldRestart().defineInRange("Inventory Buttons Offset X", 0, -1000, 1000);
-            buttonsOffsetY = BUILDER.worldRestart().defineInRange("Inventory Buttons Offset Y", 0, -1000, 1000);
+            BUILDER.push("Settings");
+            showArmorPreview = BUILDER.worldRestart().comment("If enabled, armor model will be displayed next to tooltip.").define("ArmorPreview", true);
+            renderModel = BUILDER.worldRestart().comment("If enabled, item icons will be 3D. This option is", "overridden by entries from the resource pack.").define("3DIcons", true);
+            scale = BUILDER.worldRestart().comment("Basic icon scale. This option is overridden by entries from", "the resource pack.").defineInRange("Scale", 2.0, 1, 20);
             BUILDER.pop();
             CLIENT_SPEC = BUILDER.build();
         }
