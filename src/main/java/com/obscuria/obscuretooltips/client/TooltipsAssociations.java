534e6e18fdd0ddc49fc70348af5a17f8bf30068c
Obscuria <ObscuriaLithium@gmail.com>
2023-07-03 22:31:24 +0300

    Update 2

--- src/main/java/com/obscuria/obscuretooltips/client/TooltipsAssociations.java
+++ src/main/java/com/obscuria/obscuretooltips/client/TooltipsAssociations.java
@@ -1,8 +1,8 @@
 package com.obscuria.obscuretooltips.client;
 
 import com.google.common.collect.ImmutableList;
-import com.obscuria.obscuretooltips.client.style.TooltipStylePreset;
 import com.obscuria.obscuretooltips.client.style.TooltipStyle;
+import com.obscuria.obscuretooltips.client.style.TooltipStylePreset;
 import com.obscuria.obscuretooltips.client.style.effect.TooltipEffect;
 import com.obscuria.obscuretooltips.client.style.frame.TooltipFrame;
 import com.obscuria.obscuretooltips.client.style.icon.TooltipIcon;
@@ -10,7 +10,6 @@ import com.obscuria.obscuretooltips.client.style.panel.TooltipPanel;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
-import net.minecraft.world.item.Rarity;
 import org.jetbrains.annotations.Contract;
 
 import java.util.ArrayList;
@@ -48,29 +47,37 @@ public class TooltipsAssociations {
     }
 
     public static TooltipPanel defaultPanel(ItemStack stack) {
-        if (stack.getRarity() == Rarity.EPIC) return TooltipsRegistry.PANEL_GOLDEN;
-        if (stack.getRarity() == Rarity.RARE) return TooltipsRegistry.PANEL_SILVER;
-        return TooltipsRegistry.PANEL_DEFAULT;
+        return switch (rarityName(stack)) {
+            case "EPIC", "LEGENDARY", "MYTHIC" -> TooltipsRegistry.PANEL_GOLDEN;
+            case "RARE" -> TooltipsRegistry.PANEL_SILVER;
+            default -> TooltipsRegistry.PANEL_DEFAULT;
+        };
     }
 
     public static TooltipFrame defaultFrame(ItemStack stack) {
-        if (stack.getRarity() == Rarity.EPIC) return TooltipsRegistry.FRAME_GOLDEN;
-        if (stack.getRarity() == Rarity.RARE) return TooltipsRegistry.FRAME_SILVER;
-        return TooltipsRegistry.FRAME_DEFAULT;
+        return switch (rarityName(stack)) {
+            case "EPIC", "LEGENDARY", "MYTHIC" -> TooltipsRegistry.FRAME_GOLDEN;
+            case "RARE" -> TooltipsRegistry.FRAME_SILVER;
+            default -> TooltipsRegistry.FRAME_DEFAULT;
+        };
     }
 
     public static TooltipIcon defaultIcon(ItemStack stack) {
-        if (stack.getRarity() == Rarity.EPIC) return TooltipsRegistry.ICON_ANIMATED_EPIC;
-        if (stack.getRarity() == Rarity.RARE) return TooltipsRegistry.ICON_ANIMATED;
-        if (stack.getRarity() == Rarity.UNCOMMON) return TooltipsRegistry.ICON_ANIMATED;
-        return TooltipsRegistry.ICON_DEFAULT;
+        return switch (rarityName(stack)) {
+            case "EPIC", "LEGENDARY", "MYTHIC" -> TooltipsRegistry.ICON_ANIMATED_EPIC;
+            case "UNCOMMON", "RARE" -> TooltipsRegistry.ICON_ANIMATED;
+            default -> TooltipsRegistry.ICON_DEFAULT;
+        };
     }
 
     public static List<TooltipEffect> collectEffects(ItemStack stack, ImmutableList<TooltipEffect> associated) {
         final List<TooltipEffect> effects = new ArrayList<>(associated);
         if ((stack.isEnchanted() || stack.getItem() == Items.ENCHANTED_BOOK) && !effects.contains(TooltipsRegistry.ENCHANTMENT_EFFECT))
             effects.add(TooltipsRegistry.ENCHANTMENT_EFFECT);
-        if (stack.getItem() == Items.DRAGON_EGG) effects.add(TooltipsRegistry.TAILS_EFFECT);
         return effects;
+    }
+
+    private static String rarityName(ItemStack stack) {
+        return stack.getRarity().toString().toUpperCase();
     }
 }
