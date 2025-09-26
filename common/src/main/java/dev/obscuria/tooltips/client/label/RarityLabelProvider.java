701d780e3e1219ceb675c1ddaf1d487a845c62b6
Obscuria <obscurialithium@gmail.com>
2025-09-26 15:15:45 -0300

    Fix #5

--- common/src/main/java/dev/obscuria/tooltips/client/label/RarityLabelProvider.java
+++ common/src/main/java/dev/obscuria/tooltips/client/label/RarityLabelProvider.java
@@ -5,6 +5,7 @@ import net.minecraft.ChatFormatting;
 import net.minecraft.client.gui.screens.inventory.tooltip.ClientTooltipComponent;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Rarity;
 
 public record RarityLabelProvider() implements LabelProvider {
 
@@ -18,8 +19,13 @@ public record RarityLabelProvider() implements LabelProvider {
 
     @Override
     public ClientTooltipComponent create(ItemStack stack) {
-        final var rarityName = stack.getRarity().name().toLowerCase();
-        final var component = Component.translatable("rarity." + rarityName).withStyle(ChatFormatting.GRAY);
+        final var key = makeRarityKey(stack.getRarity());
+        final var component = Component.translatable(key).withStyle(ChatFormatting.GRAY);
         return ClientTooltipComponent.create(component.getVisualOrderText());
+    }
+
+    private String makeRarityKey(Rarity rarity) {
+        final var name = rarity.name().toLowerCase().replaceAll(":", ".");
+        return "rarity." + name + ".name";
     }
 }
