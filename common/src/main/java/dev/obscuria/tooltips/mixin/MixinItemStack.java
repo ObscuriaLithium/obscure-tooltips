0d4d87dc5064e02f0a3ea3555d46fdf581bcea0b
Obscuria <obscurialithium@gmail.com>
2026-08-19 19:07:51 -0300

    fix: refactor `getTooltipImage` injection with `@ModifyReturnValue`

--- common/src/main/java/dev/obscuria/tooltips/mixin/MixinItemStack.java
+++ common/src/main/java/dev/obscuria/tooltips/mixin/MixinItemStack.java
@@ -1,28 +1,25 @@
 package dev.obscuria.tooltips.mixin;
 
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import dev.obscuria.fragmentum.content.world.tooltip.GroupTooltip;
 import dev.obscuria.tooltips.client.component.StackBuffer;
 import dev.obscuria.tooltips.config.ClientConfig;
 import net.minecraft.world.inventory.tooltip.TooltipComponent;
 import net.minecraft.world.item.ItemStack;
-import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.Optional;
 
 @Mixin(value = ItemStack.class, priority = Integer.MAX_VALUE)
+@SuppressWarnings("OptionalUsedAsFieldOrParameterType")
 public abstract class MixinItemStack {
 
-    @Inject(method = "getTooltipImage", at = @At("RETURN"), cancellable = true)
-    private void injectStackBuffer(CallbackInfoReturnable<Optional<TooltipComponent>> info) {
+    @ModifyReturnValue(method = "getTooltipImage", at = @At("RETURN"))
+    private Optional<TooltipComponent> aquamirae$injectBuffer(Optional<TooltipComponent> original) {
         final var self = (ItemStack) (Object) this;
-        if (!ClientConfig.ENABLED.get()) return;
-        if (ClientConfig.isIgnored(self.getItem())) return;
-        final @Nullable var image = info.getReturnValue().orElse(null);
-        final var group = GroupTooltip.maybeGroup(image, new StackBuffer(self));
-        info.setReturnValue(Optional.of(group));
+        if (!ClientConfig.ENABLED.get()) return original;
+        if (ClientConfig.isIgnored(self.getItem())) return original;
+        return Optional.of(GroupTooltip.maybeGroup(original.orElse(null), new StackBuffer(self)));
     }
 }
